03a161b0667b084f91468ee77f4e010898f1e909
Jafeth Garcia <jafi666@hotmail.com>
2019-02-21 21:04:53 -0400

    adding login logic

--- features/environment.py
+++ features/environment.py
@@ -7,3 +7,13 @@ config = yaml.load(open('configuration/config.yml'))
 
 def before_all(context):
     context.config = config
+    context.api_url = config['host'] + config['root_path']
+    consumer_key = config['consumer_key']
+    consumer_secret = config['consumer_secret']
+    context.auth_header = 'OAuth ' \
+                          f'oauth_consumer_key="{consumer_key}", ' \
+                          'oauth_signature_method="HMAC-SHA1", ' \
+                          'oauth_timestamp="1550794402", ' \
+                          'oauth_nonce="705p0U3tiOa", ' \
+                          'oauth_version="1.0",' \
+                          f'oauth_signature="Ft7rFulm8NF6ozQSRMUpklz4d04%3D"'
--- features/steps/other_objects_collections_smoke.py
+++ features/steps/other_objects_collections_smoke.py
@@ -1,6 +1,20 @@
+import requests
+from requests_oauthlib import OAuth1
+from compare import expect
+
+
 @given(u'I am authentication on schoology with username pepe and password pepepass')
 def step_impl(context):
-    pass
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Host': 'api.schoology.com'
+    }
+    url = f'{context.api_url}/groups'
+    auth = OAuth1(context.config['consumer_key'], context.config['consumer_secret'])
+
+    r = requests.get(url, auth=auth, headers=headers)
+    expect(r.status_code).to_equal(200)
 
 
 @then(u'I am logger into schoology')
